203aca03e1c2d4fbec46a19846efbdce36a05b07
laps78 <sovtem@gmail.com>
2024-03-13 00:12:50 +0300

    Initial commit

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+var book = {
+    name: "book name",
+    isbn: "2132139823092"
+};
---
+++ src/app.ts
@@ -0,0 +1,9 @@
+interface Book {
+    name: string;
+    isbn: string;
+}
+
+const book: Book = {
+    name: "book name",
+    isbn: "2132139823092"
+};
