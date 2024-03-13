aa5d2cda59573275046ae9294515cd31b750fd8e
laps78 <sovtem@gmail.com>
2024-03-13 04:21:19 +0300

    дополнена конфигурация компилятора ts. Файлы наолнены примерами с различной типизацией

--- dist/app.js
+++ dist/app.js
@@ -3,3 +3,34 @@ var book = {
     name: "book name",
     isbn: "2132139823092"
 };
+var max;
+max = [1, 2];
+var Season;
+(function (Season) {
+    Season[Season["Winter"] = 0] = "Winter";
+    Season[Season["Spring"] = 1] = "Spring";
+    Season[Season["Summer"] = 2] = "Summer";
+    Season[Season["Autumn"] = 3] = "Autumn";
+})(Season || (Season = {}));
+;
+var current = Season.Summer;
+var person = { name: "tom", age: 23 };
+var simpleBook = /** @class */ (function () {
+    function simpleBook() {
+    }
+    return simpleBook;
+}());
+var ComicsBook = /** @class */ (function () {
+    function ComicsBook() {
+    }
+    return ComicsBook;
+}());
+var primaryColor = "green";
+var secondaryColor = 0x8e8e8e;
+var data = "Ivan";
+var strLength = data.toString().length;
+var simpleBuilder = function (name, surname) {
+    return "Mr " + name + " " + surname;
+};
+var fullName = simpleBuilder("Bob", "Simpson");
+console.log(fullName);
--- src/app.ts
+++ src/app.ts
@@ -7,3 +7,51 @@ const book: Book = {
     name: "book name",
     isbn: "2132139823092"
 };
+
+let max: [number,number];
+max = [1,2];
+
+enum Season { Winter, Spring, Summer, Autumn };
+let current: Season = Season.Summer;
+
+let person = {name:"tom", age: 23}
+
+interface PrintedItem {
+    name: string;
+    isbn: string;
+    pagesCount: number;
+    hasHardCover: boolean;
+}
+
+class simpleBook implements PrintedItem {
+    name: string;
+    isbn: string;
+    pagesCount: number;
+    hasHardCover: true;
+}
+
+class ComicsBook implements PrintedItem {
+    name: string;
+    isbn: string;
+    pagesCount: number;
+    hasHardCover: false;
+}
+
+type ColorType = string | number;
+let primaryColor: ColorType = "green";
+let secondaryColor: ColorType = 0x8e8e8e;
+
+let data: any = "Ivan";
+const strLength: number =(<string>data).toString().length;
+
+interface FullNameBuilder {
+    (name: string, surname: string): string;
+}
+
+let simpleBuilder: FullNameBuilder = function (name: string, surname: string):string {
+    return "Mr " + name + " " + surname;
+}
+
+let fullName = simpleBuilder("Bob", "Simpson");
+console.log(fullName);
+
